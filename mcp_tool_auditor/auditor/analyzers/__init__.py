df3e3ad24affafdc718670b1c36ab8781053b92e
perparimmjeku <120132306+perparimmjeku@users.noreply.github.com>
2026-05-23 22:05:52 +0200

    Delete mcp_tool_auditor directory

--- mcp_tool_auditor/auditor/analyzers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .static import StaticAnalyzer
-from .heuristic import HeuristicAnalyzer
-from .schema import SchemaAnalyzer
-from .rugpull import RugPullDetector
-
-__all__ = ["StaticAnalyzer", "HeuristicAnalyzer", "SchemaAnalyzer", "RugPullDetector"]
--- mcp_tool_auditor/auditor/reporters/__init__.py
+++
@@ -1,4 +0,0 @@
-from .json_reporter import JSONReporter
-from .markdown_reporter import MarkdownReporter
-
-__all__ = ["JSONReporter", "MarkdownReporter"]
